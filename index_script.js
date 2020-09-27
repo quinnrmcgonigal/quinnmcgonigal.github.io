95dc24b4ccdf549f97d78f3345260e226c6a017c
quinnrmcgonigal <mcg17005@byui.edu>
2020-09-26 19:09:45 -0600

    commit

--- index_script.js
+++
@@ -1,5 +0,0 @@
-var m = "Last Updated: " + new Date(document.lastModified);
-      var p = m.length-8;
-      document.writeln("<center>");
-      document.write(m.substring(p, 0));
-      document.writeln("</center>");
